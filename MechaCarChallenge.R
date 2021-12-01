e365db14c1a6f3b15e1d8cda5915d248f3cbfc16
LaviJ <lavinajagwani@gmail.com>
2021-12-01 12:45:50 -0800

    Completion of Deliverable 2 of Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,10 +15,12 @@ lm_mechacar <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 summary(lm_mechacar)
 
 # Deliverable 2: Create Visualizations for the Trip Analysis
-# 4.Read in the CSV file
+# Step 2. Import & Read in the CSV file
 suspension_coil <- read.csv(file="../Data_MechaCar_Statistical_Analysis/Suspension_Coil.csv")
 
-# Step 3
-total_summary_df <- 
-summarize_Lotwise <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), SD_PSI=sd(PSI), .groups = 'keep')
+# Step 3. creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard
+total_summary_df <- suspension_coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep')
+
+# Step 4. lot_summary dataframe using the group_by() and the summarize() functions
+lot_summary_df <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep')
   
