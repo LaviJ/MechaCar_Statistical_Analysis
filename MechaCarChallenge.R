370f03d881f3b9c954172ca79d2ae3cb00647dec
LaviJ <lavinajagwani@gmail.com>
2021-12-04 11:56:02 -0800

    Completion of Deliverable 3 of Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -25,5 +25,16 @@ total_summary_df <- suspension_coil %>% summarize(Mean_PSI=mean(PSI), Median=med
 # Step 4 create lotwise df of the mean, median, variance, and standard deviation of the suspension coil's PSI column
 lot_summary_df <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median=median(PSI), Variance=var(PSI), SD_PSI=sd(PSI), .groups = 'keep')
 
+# Deliverable 3: T-Tests on Suspension Coils
+# Step 1 . determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+t.test(suspension_coil$PSI,mu=1500)
+
+# Step 2 . three more RScripts to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch
+# R_script 1
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+# R_script 2
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+# R_script 3
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
 
 
